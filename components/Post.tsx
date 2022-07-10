00d75dccc7d7c8fc087c16c91380e1bb5ffa4eab
Alex Ross <alxrg@me.com>
2022-07-09 23:54:58 -0500

    added prisma and basic hardcoded functionality

---
+++ components/Post.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Router from "next/router";
+import ReactMarkdown from "react-markdown";
+// import Post from "../src/pages/p/[id]";
+
+export type PostProps = {
+  id: string;
+  title: string;
+  author: {
+    name: string;
+    email: string;
+  } | null;
+  content: string;
+  published: boolean;
+};
+
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+  const authorName = post.author ? post.author.name : "Unknown author";
+  return (
+    <div onClick={() => Router.push("../src/pages/p/[id]", `../src/pages/p/${post.id}`)}>
+      <h2>{post.title}</h2>
+      <small>By {authorName}</small>
+      <ReactMarkdown children={post.content} />
+      <style jsx>{`
+        div {
+          color: inherit;
+          padding: 2rem;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Post;
