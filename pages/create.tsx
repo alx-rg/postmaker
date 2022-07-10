3166ea2adc3c05438bde0a6828de1f67731024e3
Alex Ross <alxrg@me.com>
2022-07-10 18:20:29 -0500

    create page loading and working

---
+++ pages/create.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Layout from '../components/Layout';
+import Router from 'next/router';
+import createStyle from '../styles/create.js'
+
+const Draft: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const body = { title, content };
+      await fetch('/api/post', {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(body),
+      });
+      await Router.push('/drafts');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Layout>
+      <div>
+        <form onSubmit={submitData}>
+          <h1>New Draft</h1>
+          <input
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            type="text"
+            value={title}
+          />
+          <textarea
+            cols={50}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Content"
+            rows={8}
+            value={content}
+          />
+          <input disabled={!content || !title} type="submit" value="Create" />
+          <a className="back" href="#" onClick={() => Router.push('/')}>
+            Cancel
+          </a>
+        </form>
+      </div>
+      <style jsx>
+        {createStyle}
+      </style>
+    </Layout>
+  );
+};
+
+export default Draft;
